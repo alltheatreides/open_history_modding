9e353c471d33dc7b97ed5834ccd2a7b954083fab
alltheatreides <goldschmidt.antoine@gmail.com>
2022-04-28 11:45:40 +0200

    Content: Province Search Basic Implementation

---
+++ src/components/ProvinceCard.vue
@@ -0,0 +1,115 @@
+<script setup>
+defineProps({
+   province_id: {
+      type: Number,
+      required: false,
+   },
+   barony: {
+      type: String,
+      required: false,
+   },
+   culture: {
+      type: String,
+      required: false,
+   },
+   religion: {
+      type: String,
+      required: false,
+   },
+   date: {
+      type: String,
+      required: false,
+   },
+   buildings: {
+      type: [Array],
+      required: false,
+   },
+   special_buildings: {
+      type: String,
+      required: false,
+   },
+   duchy_capital_building: {
+      type: String,
+      required: false,
+   },
+   special_building_slot: {
+      type: String,
+      required: false,
+   },
+   terrain: {
+      type: String,
+      required: false,
+   },
+   author: {
+      type: String,
+      required: false,
+   },
+   manual: {
+      type: Boolean,
+      required: false,
+   },
+});
+</script>
+
+<template>
+   <div
+      class="flex flex-col gap-4 bg-[#ede7d9] p-6 rounded-xl w-[350px] border-b-2 border-gray-800"
+   >
+      <div class="flex gap-2 text-normal border-b-2 border-secondary">
+         <p class="font-semibold">Date:</p>
+         <p class="">{{ date }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Province ID:</p>
+         <h3 class="text-normal">{{ province_id }}</h3>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Title:</p>
+         <h4 class="">{{ barony }}</h4>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Culture:</p>
+         <p class="">{{ culture }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Religion:</p>
+         <p class="">{{ religion }}</p>
+      </div>
+      <div class="">
+         <p class="text-normal font-semibold">Buildings:</p>
+         <div class="mt-2 flex flex-col gap-2">
+            <p class="" v-for="building in buildings">
+               {{ building }}
+            </p>
+         </div>
+      </div>
+      <div class="">
+         <p class="text-normal font-semibold">Special Buildings:</p>
+         <div class="flex flex-col gap-2">
+            <p class="" v-for="special_building in special_buildings">
+               {{ special_building }}
+            </p>
+         </div>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Duchy Capital Building:</p>
+         <p class="">{{ duchy_capital_building }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Special Building Slot:</p>
+         <p class="">{{ special_building_slot }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Terrain:</p>
+         <p class="">{{ terrain }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Author:</p>
+         <p class="">{{ author }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Manually added:</p>
+         <p class="">{{ manual }}</p>
+      </div>
+   </div>
+</template>
--- src/views/SearchHistory.vue
+++ src/views/SearchHistory.vue
@@ -1,10 +1,56 @@
 <script setup>
 import searchIcon from "../assets/svg/search_icon.svg";
+import { supabase } from "../supabase/supabase";
+import ProvinceCard from "../components/ProvinceCard.vue";
+</script>
+
+<script>
+export default {
+   data() {
+      return {
+         searchInput: "",
+         searchResults: [],
+      };
+   },
+   methods: {
+      async supabaseQueryProvince() {
+         try {
+            let { data, error, status } = await supabase
+               .from("province_history")
+               .select(
+                  "province_id, holding_type, religion, culture, date, barony, buildings, special_buildings, terrain, duchy_capital_building, special_building_slot, manual, author"
+               )
+               .textSearch("barony", this.searchInput);
+            this.searchResults = data;
+            console.log(status, error);
+            // console.log(data);
+            console.log(this.searchResults);
+            console.log(this.searchResults[0].province_id);
+         } catch (error) {
+            console.log(error.message);
+         } finally {
+         }
+         // try {
+         //    let { data, error, status } = await supabase
+         //       .from("province_history")
+         //       .select(
+         //          `province_id, holding_type, religon, culture, date, barony, buildings, special_buildings, terrain, duchy_capital_building, special_building_slot, author, landed_title!inner(title)`
+         //       )
+         //       .textSearch("landed_title.title", this.searchInput);
+         //    console.log(status);
+         //    console.log(data);
+         // } catch (error) {
+         //    console.log(error.message);
+         // } finally {
+         // }
+      },
+   },
+};
 </script>
 
 <template>
    <main class="container mx-auto mb-10">
-      <section class="lg:w-4/6 mx-auto mt-40">
+      <section class="lg:w-4/6 mx-auto mt-20">
          <h1 class="text-7xl text-center">Search vanilla history</h1>
          <!-- Search bar -->
          <article
@@ -77,17 +123,42 @@ import searchIcon from "../assets/svg/search_icon.svg";
             </div>
             <div class="flex-1 h-full">
                <input
+                  v-model="searchInput"
                   type="text"
-                  placeholder="Start searching for a province history"
+                  placeholder="Search for a province history from its barony name"
                   class="border-0 bg-secondary10 w-full px-5 py-3.5 font-medium text-xl focus:ring-0"
+                  @keyup.enter="supabaseQueryProvince"
                />
             </div>
             <img
                :src="searchIcon"
                alt="Search icon"
-               class="bg-secondary p-4 h-full"
+               class="bg-secondary p-4 h-full cursor-pointer"
+               @click="supabaseQueryProvince"
             />
          </article>
+      </section>
+      <!-- Results of search -->
+      <section class="mt-16 flex gap-6 w-full mx-auto flex-wrap justify-start">
+         <div v-if="searchResults.length <= 0 && searchInput !== ''">
+            <p class="font-semibold">No results were found</p>
+         </div>
+         <div v-for="result in searchResults" :key="result.province_id">
+            <ProvinceCard
+               :province_id="result.province_id"
+               :barony="result.barony"
+               :date="result.date"
+               :culture="result.culture"
+               :religion="result.religion"
+               :buildings="result.buildings"
+               :special_buildings="result.special_buildings"
+               :duchy_capital_building="result.duchy_capital_building"
+               :special_building_slot="result.special_building_slot"
+               :terrain="result.terrain"
+               :author="result.author"
+               :manual="result.manual"
+            />
+         </div>
       </section>
    </main>
 </template>
