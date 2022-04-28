916f507c792dc83c90889d2b40c5ab010deed78d
alltheatreides <goldschmidt.antoine@gmail.com>
2022-04-28 14:08:45 +0200

    Content: Title History Search Basic Implementation

---
+++ src/components/TitleCard.vue
@@ -0,0 +1,91 @@
+<script setup>
+defineProps({
+   title: {
+      type: String,
+      required: false,
+   },
+   date: {
+      type: String,
+      required: false,
+   },
+   liege: {
+      type: String,
+      required: false,
+   },
+   holder: {
+      type: Number,
+      required: false,
+   },
+   government: {
+      type: String,
+      required: false,
+   },
+   change_development_level: {
+      type: String,
+      required: false,
+   },
+   succession_laws: {
+      type: [Array],
+      required: false,
+   },
+   de_jure_liege: {
+      type: String,
+      required: false,
+   },
+   insert_title_history: {
+      type: String,
+      required: false,
+   },
+   holder_ignore_head_of_faith_requirement: {
+      type: String,
+      required: false,
+   },
+});
+</script>
+
+<template>
+   <div
+      class="flex flex-col gap-4 bg-[#ede7d9] p-6 rounded-xl w-[350px] border-b-2 border-gray-800"
+   >
+      <div class="flex gap-2 text-normal border-b-2 border-secondary">
+         <p class="font-semibold">Date:</p>
+         <p class="">{{ date }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Title:</p>
+         <h3 class="text-normal">{{ title }}</h3>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Liege:</p>
+         <h4 class="">{{ liege }}</h4>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Holder:</p>
+         <p class="">{{ holder }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Government:</p>
+         <p class="">{{ government }}</p>
+      </div>
+      <div class="">
+         <p class="text-normal font-semibold">Succession Laws:</p>
+         <div class="mt-2 flex flex-col gap-2">
+            <p class="" v-for="succession_law in succession_laws">
+               {{ succession_law }}
+            </p>
+         </div>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">De Jure Liege:</p>
+         <p class="">{{ de_jure_liege }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Insert Title History:</p>
+         <p class="">{{ insert_title_history }}</p>
+      </div>
+      <div class="flex gap-2 text-normal">
+         <p class="font-semibold">Holder ignore head of faith requirement:</p>
+         <p class="">{{ holder_ignore_head_of_faith_requirement }}</p>
+      </div>
+   </div>
+</template>
--- src/views/SearchHistory.vue
+++ src/views/SearchHistory.vue
@@ -2,6 +2,7 @@
 import searchIcon from "../assets/svg/search_icon.svg";
 import { supabase } from "../supabase/supabase";
 import ProvinceCard from "../components/ProvinceCard.vue";
+import TitleCard from "../components/TitleCard.vue";
 </script>
 
 <script>
@@ -10,6 +11,7 @@ export default {
       return {
          searchInput: "",
          searchResults: [],
+         selectedSearchCategory: "",
       };
    },
    methods: {
@@ -24,25 +26,33 @@ export default {
             this.searchResults = data;
             console.log(status, error);
             // console.log(data);
-            console.log(this.searchResults);
-            console.log(this.searchResults[0].province_id);
+            // console.log(this.searchResults);
+            // console.log(this.searchResults[0].province_id);
          } catch (error) {
             console.log(error.message);
          } finally {
          }
-         // try {
-         //    let { data, error, status } = await supabase
-         //       .from("province_history")
-         //       .select(
-         //          `province_id, holding_type, religon, culture, date, barony, buildings, special_buildings, terrain, duchy_capital_building, special_building_slot, author, landed_title!inner(title)`
-         //       )
-         //       .textSearch("landed_title.title", this.searchInput);
-         //    console.log(status);
-         //    console.log(data);
-         // } catch (error) {
-         //    console.log(error.message);
-         // } finally {
-         // }
+      },
+      async supabaseQueryTitle() {
+         try {
+            let { data, error, status } = await supabase
+               .from("title_history")
+               .select("*")
+               .textSearch("title", this.searchInput);
+            this.searchResults = data;
+            console.log(status, error);
+            console.log(data);
+         } catch (error) {
+            console.log(error.message);
+         } finally {
+         }
+      },
+      searchSupabase() {
+         if (this.selectedSearchCategory === "Provinces") {
+            this.supabaseQueryProvince();
+         } else if (this.selectedSearchCategory === "Titles") {
+            this.supabaseQueryTitle();
+         }
       },
    },
 };
@@ -58,91 +68,49 @@ export default {
          >
             <!-- Drop down menu -->
             <div class="h-full">
-               <!-- Drop down button -->
-               <button
-                  id="dropdownDefault"
-                  data-dropdown-toggle="dropdown"
-                  class="text-tertiary bg-secondary focus:ring-4 focus:outline-none focus:ring-grey-300 font-medium text-xl px-5 py-3.5 text-center inline-flex items-center"
-                  type="button"
+               <!-- Drop Down select -->
+               <select
+                  v-model="selectedSearchCategory"
+                  class="text-tertiary bg-secondary focus:outline-none font-medium text-xl px-5 py-3.5 text-center inline-flex items-center focus:ring-0 border-0 focus:border-0 pr-10"
+                  name="select"
                >
-                  Provinces
-                  <svg
-                     class="w-5 h-5 ml-2"
-                     fill="none"
-                     stroke="#FFD263"
-                     viewBox="0 0 24 24"
-                     xmlns="http://www.w3.org/2000/svg"
-                  >
-                     <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="4"
-                        d="M19 9l-7 7-7-7"
-                     ></path>
-                  </svg>
-               </button>
-               <!-- Dropdown options -->
-               <div
-                  id="dropdown"
-                  class="z-10 hidden bg-secondary divide-y divide-gray-100 rounded shadow w-44"
-               >
-                  <ul
-                     class="py-1 text-tertiary"
-                     aria-labelledby="dropdownDefault"
-                  >
-                     <li>
-                        <a
-                           href="#"
-                           class="block px-4 py-2 hover:bg-tertiary hover:text-secondary"
-                           >Landed Titles</a
-                        >
-                     </li>
-                     <li>
-                        <a
-                           href="#"
-                           class="block px-4 py-2 hover:bg-tertiary hover:text-secondary"
-                           >Title History</a
-                        >
-                     </li>
-                     <li>
-                        <a
-                           href="#"
-                           class="block px-4 py-2 hover:bg-tertiary hover:text-secondary"
-                           >Province History</a
-                        >
-                     </li>
-                     <!-- <li>
-                        <a
-                           href="#"
-                           class="block px-4 py-2 hover:bg-tertiary hover:text-secondary"
-                           >Character History</a
-                        >
-                     </li> -->
-                  </ul>
-               </div>
+                  <option disabled value="">Category</option>
+                  <option value="Provinces" selected>Provinces</option>
+                  <option value="Titles">Titles</option>
+               </select>
             </div>
+            <!-- Search input -->
             <div class="flex-1 h-full">
                <input
                   v-model="searchInput"
                   type="text"
-                  placeholder="Search for a province history from its barony name"
+                  placeholder="Search for an entry from its title name"
                   class="border-0 bg-secondary10 w-full px-5 py-3.5 font-medium text-xl focus:ring-0"
-                  @keyup.enter="supabaseQueryProvince"
+                  @keyup.enter="searchSupabase"
                />
             </div>
+            <!-- Search icon -->
             <img
                :src="searchIcon"
                alt="Search icon"
                class="bg-secondary p-4 h-full cursor-pointer"
-               @click="supabaseQueryProvince"
+               @click="searchSupabase"
             />
          </article>
       </section>
-      <!-- Results of search -->
-      <section class="mt-16 flex gap-6 w-full mx-auto flex-wrap justify-start">
-         <div v-if="searchResults.length <= 0 && searchInput !== ''">
-            <p class="font-semibold">No results were found</p>
+      <!-- Results of search province -->
+      <section
+         v-if="selectedSearchCategory === 'Provinces'"
+         class="mt-16 flex gap-6 w-full mx-auto flex-wrap justify-start"
+      >
+         <!-- In case of no results -->
+         <div
+            class="w-full mx-auto"
+            v-if="searchResults.length <= 0 && searchInput !== ''"
+         >
+            <p class="font-semibold text-center">No results were found</p>
          </div>
+         <!-- Search results div holder for province -->
          <div v-for="result in searchResults" :key="result.province_id">
             <ProvinceCard
                :province_id="result.province_id"
@@ -157,6 +125,36 @@ export default {
                :terrain="result.terrain"
                :author="result.author"
                :manual="result.manual"
+            />
+         </div>
+      </section>
+      <!-- Results of search for title -->
+      <section
+         v-if="selectedSearchCategory === 'Titles'"
+         class="mt-16 flex gap-6 w-full mx-auto flex-wrap justify-start"
+      >
+         <!-- In case of no results -->
+         <div
+            class="w-full mx-auto"
+            v-if="searchResults.length <= 0 && searchInput !== ''"
+         >
+            <p class="font-semibold text-center">No results were found</p>
+         </div>
+         <!-- Search results div holder for province -->
+         <div v-for="result in searchResults" :key="result.id">
+            <TitleCard
+               :title="result.title"
+               :date="result.date"
+               :liege="result.liege"
+               :holder="result.holder"
+               :government="result.government"
+               :change_development_level="result.change_development_level"
+               :succession_laws="result.succession_laws"
+               :de_jure_liege="result.de_jure_liege"
+               :insert_title_history="result.insert_title_history"
+               :holder_ignore_head_of_faith_requirement="
+                  result.holder_ignore_head_of_faith_requirement
+               "
             />
          </div>
       </section>
