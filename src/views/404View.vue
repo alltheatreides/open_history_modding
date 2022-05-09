d2189b6d12fa00ad9039a1389ea7738bd9ea3ce9
alltheatreides <goldschmidt.antoine@gmail.com>
2022-05-09 17:24:43 +0200

    Content: Added 404 page

---
+++ src/views/404View.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+// reactive state
+
+// lifecycle hooks
+onMounted(() => {});
+</script>
+
+<template>
+   <main class="container mx-auto mb-10">
+      <section class="text-center">
+         <h1 class="text-2xl">404 Page</h1>
+         <p>Looks like you took a wrong turn</p>
+      </section>
+   </main>
+</template>
