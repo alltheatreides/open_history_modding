95a61cf5f91b03e217cd177126249eed38f189f5
alltheatreides <goldschmidt.antoine@gmail.com>
2022-05-11 10:50:18 +0200

    Content: Removed About Page for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -49,14 +49,14 @@ const router = createRouter({
       name: 'createHistory',
       component: CreateHistory
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
