7a4f32c559f4c8d5d244a0b1c29d53b71e96f34a
alltheatreides <goldschmidt.antoine@gmail.com>
2022-05-11 16:08:13 +0200

    Content: Composition API Refactor

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,41 +1,35 @@
 <script setup>
+import { ref, onMounted, watch } from "vue";
 import { supabase } from "../supabase/supabase";
 import router from "../router";
-</script>
 
-<script>
-export default {
-   data() {
-      return {
-         email: "",
-         userName: "",
-         modTeam: "",
-         password: "",
-         errorMsg: "",
-      };
-   },
-   methods: {
-      async signUpSupabase() {
-         try {
-            const { user, session, error } = await supabase.auth.signUp({
-               email: this.email,
-               password: this.password,
-            });
-            console.log(user);
-            console.log(session);
-            if (error) {
-               this.errorMsg = error;
-               throw error;
-            } else {
-               router.push("/login");
-            }
-         } catch (error) {
-            console.log(error.message);
-         } finally {
-         }
-      },
-   },
-};
+// reactive state
+const email = ref("");
+const userName = ref("");
+const modTeam = ref("");
+const password = ref("");
+const errorMsg = ref("");
+
+// Page Methods
+async function signUpSupabase() {
+   try {
+      const { user, session, error } = await supabase.auth.signUp({
+         email: email.value,
+         password: password.value,
+      });
+      console.log(user);
+      console.log(session);
+      if (error) {
+         errorMsg.value = error;
+         throw error;
+      } else {
+         router.push("/login");
+      }
+   } catch (error) {
+      console.log(error.message);
+   } finally {
+   }
+}
 </script>
 
 <template>
@@ -83,7 +77,7 @@ export default {
             </div>
 
             <p class="mt-4 text text-red-600">
-               <span class="font-medium">{{ errorMsg }}</span>
+               <span class="font-medium">{{ errorMsg.message }}</span>
             </p>
 
             <!-- Confirmation button -->
